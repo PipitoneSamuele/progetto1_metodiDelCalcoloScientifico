efb4d93aadd85fd6c0b9a3f910eb81381c20ec6f
marco_campione <m.campione3@campus.unimib.it>
2023-04-16 16:58:35 +0200

    Setting delle variabili

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 from scipy.io import mmread
 
-a = mmread("libreria_s_pipitone_830595_py//resources//data//spa1.mtx")
+a = mmread("resources//data//spa1.mtx")
 
-print(a.A)
+# x: vettore della soluzione esatta inizializzato a tutti 1
+x = [1] * len(a.A) 
+
+# b: vettore della soluzione del sistema
+b = a * x 
+
+# tol: lista delle tolleranze con il quale testare i vari metodi
+tol = [10**-4, 10**-6, 10**-8, 10**-10]
+
+# calcolo delle soluzioni approssimate TODO
+
+# calcolo degli errori relativi, numero iterazioni e tempo di calcolo TODO
+
+print(x)
+print(b)
